2bba76ac0a7f552602e3d534c69b6911b5cd028c
Komang Krisnanda <officialkrisnandha@gmail.com>
2020-05-29 07:57:14 +0800

    Create author controller

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index(){
+        
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function show($id){
+
+    }
+
+    public function update(Request $request, $id){
+
+    }
+
+    public function destroy($id){
+        
+    }
+}
