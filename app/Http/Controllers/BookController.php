3b7f3aaad9d80ad49d969f6911427ea20e836f76
Komang Krisnanda <officialkrisnandha@gmail.com>
2020-05-29 16:41:34 +0800

    Add book & author controller

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Author;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class BookController extends Controller
+{
+    use ApiResponser;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index(){
+
+    }
+
+    public function show($id){
+
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function update(Request $request, $id){
+
+    }
+
+    public function destroy($id){
+
+    }
+}
