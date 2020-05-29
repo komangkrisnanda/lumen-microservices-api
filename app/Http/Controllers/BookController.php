8d80965b8dfa290a8ca29d3a5badebd54fa3c133
Komang Krisnanda <officialkrisnandha@gmail.com>
2020-05-29 16:17:08 +0800

    Add book controller

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index(){
+
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function update(Request $request, $id){
+
+    }
+
+    public function destroy($id){
+
+    }
+}
