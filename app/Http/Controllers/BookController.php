69d96f36860d5475ca25872a93d780ea3afbb690
Komang Krisnanda <officialkrisnandha@gmail.com>
2020-05-30 10:04:13 +0800

    Add book api services

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -1,16 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Author;
+use App\Book;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use App\Service\BookService;
-
+use App\Services\BookService;
+use App\Traits\ApiResponser;
 class BookController extends Controller
 {
     use ApiResponser;
 
-    public $authorService;
+    public $bookService;
     /**
      * Create a new controller instance.
      *
@@ -22,22 +22,22 @@ class BookController extends Controller
     }
 
     public function index(){
-
+        return $this->successResponse($this->bookService->obtainBooks());
     }
 
     public function show($id){
-
+        return $this->successResponse($this->bookService->obtainBook($id));
     }
 
     public function store(Request $request){
-
+        return $this->successResponse($this->bookService->createBook($request->all()),  Response::HTTP_CREATED);
     }
 
     public function update(Request $request, $id){
-
+        return $this->successResponse($this->bookService->updateBook($request->all(), $id));
     }
 
     public function destroy($id){
-
+        return $this->successResponse($this->bookService->deleteBook($id));
     }
 }
--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -12,4 +12,34 @@ class BookService{
     public function __construct(){
         $this->baseUri = config('services.books.base_uri');
     }
+
+    // Obtain the full list of book from the book service
+
+    public function obtainBooks(){
+        return $this->performRequest('GET', '/books');
+    }
+
+    // Create book using the book service
+
+    public function createBook($data){
+        return $this->performRequest('POST', '/books', $data);
+    }
+
+    // Obtain one list of book from the book service
+
+    public function obtainBook($id){
+        return $this->performRequest('GET', "/books/{$id}");
+    }
+
+    // Update book using the book service
+
+    public function updateBook($data, $id){
+        return $this->performRequest("PUT", "/books/{$id}", $data);
+    }
+
+    // Delete book using the book service
+
+    public function deleteBook($id){
+        return $this->performRequest("DELETE", "/books/{$id}");
+    }
 }
